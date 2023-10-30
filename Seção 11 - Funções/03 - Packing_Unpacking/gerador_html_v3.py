aae18b02c7766a286d45c47828d8e3b5f9b5e656
rogermsarmento <rogerms@ifce.edu.br>
2023-10-30 15:52:34 -0300

    Continuação da Seção 11 - Funções. Parando em Callable.

--- Seção 11 - Funções/03 - Packing_Unpacking/gerador_html_v3.py
+++ Seção 11 - Funções/03 - Packing_Unpacking/gerador_html_v3.py
@@ -11,11 +11,11 @@ def tag_bloco(conteudo, classe='success', inline=False):
 
 def tag_lista(*itens):
     # Não precismos nem de generatuions e nem de list compreheson
-    # lista = ''.join(f'<li>{item}</li>' for item in itens)
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
     # Usando os () temos um GENERATION
     # lista = ''.join((f'<li>{item}</li>' for item in itens))
     # Usando os [] temos uma LIST COMPREHESION
-    lista = ''.join([f'<li>{item}</li>' for item in itens])
+    # lista = ''.join([f'<li>{item}</li>' for item in itens])
     return f'<ul>{lista}</ul>'
     '''
     Nesta função temos um generation que para cada item que recebermos da 
@@ -28,10 +28,12 @@ def tag_lista(*itens):
 
 
 if __name__ == '__main__':
-    print(tag_bloco('bloco'))
-    print(tag_bloco('inline e classe', 'info', True))
+    # print(tag_bloco('bloco'))
+    # print(tag_bloco('inline e classe', 'info', True))
     # nesse ponto invertemos a ordem dos parâmetros!
-    print(tag_bloco('inline', inline=True))
-    print(tag_bloco(inline=True, conteudo='inline'))
-    print(tag_bloco('falhou', classe='error'))
+    # print(tag_bloco('inline', inline=True))
+    # print(tag_bloco(inline=True, conteudo='inline'))
+    # print(tag_bloco('falhou', classe='error'))
+
+    print(tag_lista('item 1', 'item 2'))
     print(tag_bloco(tag_lista('item 1', 'item 2'), classe='info'))
